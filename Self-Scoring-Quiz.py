6d4fb957ecaf69feec622928ce31c1e463e90032
JS <Jocelynn.Sergent@ela-academy.org>
2025-12-08 23:27:00 -0500

    SELF SCORING QUIZ 2

---
+++ Self-Scoring-Quiz.py
@@ -0,0 +1,37 @@
+print("What is the color of the sea?")
+print("A. Red")
+print("B. Yellow")
+print("C. Blue")
+print("D. None of the above")
+
+anwser = input("Choose A, B, C, or D: ")
+if anwser == "C":
+    print("Correct!")
+else: 
+    print("Wrong try again")
+    
+
+    score = 0
+    if anwser == "C":
+        score = score + 1
+
+
+
+             
+
+        
+print("What is the best chocolate? (2 extra points for right anwser)")
+print("A. Reeses")
+print("B. Milky way")
+print("C. Pistacio chocolate")
+print("D. Dove")
+
+anwser = input("Choose wisely")
+if anwser == "D":
+    print("Very good")
+else:
+    print("Wrong try again")
+
+    score = 0
+    if anwser == "D":
+        score = score + 3
---
+++ username.py
@@ -0,0 +1,3 @@
+for question_number in range(3):
+    anwser = input("Your anwser: ")
+    print("You said", anwser)
