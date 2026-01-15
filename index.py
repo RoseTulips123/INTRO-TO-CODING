d984526463306d42a4c35d10eb606bba34d57a9b
JS <Jocelynn.Sergent@ela-academy.org>
2026-01-15 16:53:15 -0500

    Treasure Quest scenes (to be finished)

---
+++ index.py
@@ -0,0 +1,60 @@
+# Treasure Quest 
+
+# Intro-
+
+# You are a treasure hunter who is on a quest to find hidden treasures left by four ancient kings, its been hidden for centuries and no one has been able to find it, but you wont give up! 
+# The only clue the ancient kings left behind was a stone tablet with different symbols on it, you remember seeing these symbols on your grandmothers vase that she never let you touch.
+# One day, while she was out getting groceries to make cookies, you decided to climb on top of the counter to get the vase down, you grabbed the vase and carefully climb down, you inpect the
+# vase and the four symbols on it, you look closer at the small writing underneath each symbol, they read: North (Treasure of Winter), East(Treasure of autumn), South(Treasure of spring) 
+# and west (treasure of summer), these are directions! You put the vase back then decide to set out on your quest to find the hidden treasures!
+
+
+# Game starts
+
+# Type in the number to the choice you want to make.
+
+# What direction should go to?
+# 1. North (Treasure of Winter)
+# 2. East  (Treasure of Autumn)
+# 3. South (Treasure of Spring)
+# 4. West  (Treasure of Summer)
+
+
+# (User picks North)
+#You set sail to the North!
+
+# after days of sailing you reach an island directly North of your home, you dock your ship and step onto the island, emediatly you feel huge gusts of cold, arctic winds hit you face, good thing
+# you prepared for this, you walk inland and see a huge mountain in the distance, you start to make your way to the mountain as big as mount everest, after hours of hiking you reach the base of the mountain,
+# you see a cave entrance, you brush off some snow from a fallen boulder and see a blue gemstone engraved in the rock, you enter the cave, its dark and cold, you walk deeper into the cave until you reach 
+# you see a peice of meat on the ground, you are very hungry from your voyage, do you eat the meat?
+
+# 1. Eat the meat
+# 2. Move on into the cave
+
+# (User picks 1)
+# You go to pick up the meat, as you do a huge polar bear jumps out from the shadows and attacks you, you try to fight it off but it overpowers you. 
+
+# you have failed your quest. (Loop back to entering the cave)
+
+# (User picks 2)
+# You ignore the meat and walk deeper into the cave, after a few minutes you see a light in the distance, you walk towards it and find a large chamber with the floor frozen over,
+# you see a large ice pedestal in the center of the chamber with a chest on top of it, should you cross the frozen floor to get to the chest? Or look around the chamber first?
+
+# 1. Cross the frozen floor
+# 2. look closer at the ice
+
+# (User picks 1)
+# You make your way across the frozen floor, as you near the pedestal you hear a cracking sound, before you can react the ice breaks beneath you, and you fall into freezing water below, 
+# the freezing temperature of the water gives you hypothermia and you drown.
+
+# you have failed your quest. (Loop back to entering the cave)
+
+# (User picks 2)
+# You observe the ice closely and see that the floor is very thin in some spots, you carefully make your way across the chamber avoiding the thin ice,
+# you reach the pedestal and open the chest, inside you find gold, diamonds, and a Blue gemstone!
+
+# Congratulations! You have found the Treasure of Winter! What direction would you like to go to next?
+
+# East  (Treasure of Autumn)
+# South (Treasure of Spring)
+# West  (Treasure of Summer)
