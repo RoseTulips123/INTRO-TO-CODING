7d595ec027bd3bbd5efc466f57a3dddd9f954fc3
JS <Jocelynn.Sergent@ela-academy.org>
2026-01-16 18:01:00 -0500

    master

--- index.py
+++ index.py
@@ -51,10 +51,88 @@
 
 # (User picks 2)
 # You observe the ice closely and see that the floor is very thin in some spots, you carefully make your way across the chamber avoiding the thin ice,
-# you reach the pedestal and open the chest, inside you find gold, diamonds, and a Blue gemstone!
+# you reach the pedestal and open the chest, inside you find gold, diamonds, a sheild, and a Blue gemstone!
 
 # Congratulations! You have found the Treasure of Winter! What direction would you like to go to next?
 
-# East  (Treasure of Autumn)
-# South (Treasure of Spring)
-# West  (Treasure of Summer)
+# 1. East  (Treasure of Autumn)
+# 2. South (Treasure of Spring)
+# 3. West  (Treasure of Summer)
+
+# (User picks East)
+# You set sail to the East!
+
+# Upon arriving at the island directly East of your home, you immediately see beautiful mountainous terrain and colorful trees, you see two paths ahead
+# of you, one leading into a beautiful forest, and one leading towards a treehouse village built into the mountainside, which path do you take?
+
+# 1. Enter the forest
+# 2. Head towards the treehouse village
+
+# (User picks 1)
+# You enter the forest, the trees are tall and the leaves are a beautiful array of reds, oranges, and yellows, as you walk deeper into the forest you
+# see a clearing ahead, in the middle of the clearing you see an injured rare fox thats colored gold and red, do you help the fox, ignore it or take
+# its fur for profit
+
+# 1. Help the fox
+# 2. Take its fur
+# 3. Ignore it
+
+# (User picks 1)
+# You carefully approach the injured fox, it looks at you with pleading eyes, you reach into you bag to find some bandages and medicine,
+# luckily you packed extra supplies for your journey, you carefully bandage the fox's wounds and give it some water, after a few minutes the fox
+# gets up and  hands you what it looks like a small map, the fox then runs off into the forest, you look at the map and see that it shows an "x"
+# marking to a spot deeper in the forest, you decide to follow the map, after a few hours of searching you reach the spot marked on the map, you finnaly
+# find the spot, you start digging and find a chest buried in the ground, you open the chest and find gold, jewels, a cape, and a red gemstone!
+
+# Congratulations! You have found the Treasure of Autumn! What direction would you like to go to next?
+
+# (User picks 2)
+# You see the foxes fur would be worth a ton of gold, you quickly grab your knife and skin the fox, you put the fur in your bag and turn back
+# towards the path, as you walk back, a gigantic tree falls beside you, and before you could run, it crushes you.
+
+# you have failed your quest. (Loop back to entering the forest)
+
+# (User picks 3)
+# You decide to ignore the fox and continue on your way, as you walk deeper into the forest, suddenly an tribe of hostile natives ambush you,
+# they overpower you and take all your supplies, leaving you stranded in the forest with no food or water, after days of wandering you find a treehouse
+# village (continue to treehouse village)
+
+# (User picks 2. treehouse village)
+# You decide that going to the treehouse village might have a better chance of finding the treasure, you make your way towards the village, 
+# as you get closer you see that the village is bustling with activity, people are going about their daily lives, you approach one of the villagers,
+# they look at you like they've seen an alien, how should you talk to them?
+
+# 1. Try to communicate with gestures
+# 2. Attempt to speak their language
+# 3. Offer them a gift from your homeland
+
+# (User picks 1)
+# You try to communicate with gestures, the villager looks confused but seems to understand you want to say, they motion for you to follow them,
+# they lead you to the village elder, the elder listens to your story and decides to help you, they give you a map with an "x" marking a spot
+# deep in the nearby forest, you follow the map and after a few hours of searching you reach the spot marked on the map, you start digging and find
+# a chest buried in the ground, inside you find gold, jewels, a cape, and a red gemstone!
+
+# Congratulations! You have found the Treasure of Autumn! What direction would you like to go to next?
+
+# (User picks 2)
+# You attempt to speak their language, but you only manage to butcher a few words, the villager looks at you angrily and calls for others,
+# soon you are surrounded by hostile villagers, they attack and kidnap you, you are held captive for weeks before you manage to escape,
+# but you are left with nothing but the clothes on your back, you eventually make your way back home dehydrated, starved, exhausted, and empty handed, 
+# you give up.
+
+# you have failed your quest. (Loop back to arriving at the island)
+
+# (User picks 3)
+# You offer the villager a gift from your homeland, they look at the gift curiously, after examining it they smile and motion for you to follow them,
+# they lead you to their hut, they give you food and water, you explain your quest to them through gestures, they take you to the village elder,
+# the villager explains your story to the elder, the elder decides to help you, they give you a map with an "x" marking a spot deep in the nearby forest,
+# you follow the map and after a few hours of searching you reach the spot marked on the map, you start digging and find achest buried in the ground,
+# inside you find gold, jewels, a cape, and a red gemstone!
+
+# Congratulations! You have found the Treasure of Autumn! What direction would you like to go to next?
+# 1. South (Treasure of Spring)
+# 2. West  (Treasure of Summer)
+
+
+
+
