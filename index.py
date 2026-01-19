4aeb45505cde01564083174f706d8269bd85589d
JS <Jocelynn.Sergent@ela-academy.org>
2026-01-18 20:57:28 -0500

    Treasure Quest scenes (COMPLETE)

--- index.py
+++ index.py
@@ -238,15 +238,105 @@
 # You set sail to the West!
 
 # You decide that your final adventure will be the western island (Or whatever the user ends on), located directly west of your home, after a few days
-# of sailing you reach the island, you dock you ship and step onto the sandy desert, you see nothing but sand dunes and cacti as far as the eye can see,
-# the sun is blazing hot, you feel thirsty by just looking at the dry desert, you  
+# of sailing you reach the island, you dock your ship and step onto the sandy desert, you see nothing but sand dunes and cacti as far as the eye can see,
+# the sun is blazing hot, you feel thirsty by just looking at the dry desert, you scan the desert for any signs of life, you see a rocky formation
+# in the distance, you decide to head towards it, hoping to find some shade from the scorching sun, you make your way towards the rocky formation,
+# When you get there, you see its a large mesa, with a few desert trees and plants, around it, you take shelter underneath one of the trees,
+# you spot a group of arabs riding camels in the distance, you walk towards them, hoping they can help you, they see you and stop their camels,
+# you luckily speak a good amount of their language from your previous travels, you explain your quest to them, they say that they can help you but
+# first you must give them something in return, what do you give them?
 
+# 1. Gold from previous treasures
+# 2. Your compass
+# 3. Your weapons
 
+# (User picks 1)
+# You reach inside your backpack and hand them the gold from previous adventures, they nod in agreement and invite you to ride on one of their camels,
+# you hop on one, and they take you to an anceint temple, they tell you its filled with various traps and puzzles and no one has ever dared to step inside.
+# you thanked them and said goodbye and stepped inside the temple, the door closes from behind you, inside the temple is a stone with anceint arab
+# words written on it, it reads, "In the heat I stand both day and night, yet I never sweat, though the sun is bright, I point the way with shadows long-
+# guess my name and you prove strong," you take a minute to process the question, then come up with the anwser-
 
+# 1. A camel
+# 2. A desert well
+# 3. A sundial
 
+# (User picks camel)
+# Suddenly, a bunch of camels came out of a compartment and attacked you, luckily you brung some weapons with you, you attack them, they run off into
+# a secret room, it closes behind them. (Continue to sundial path)
 
+# (User picks desert well)
+# Suddendly, the temple starts filling with water, you try to break the doors to get out,
+# but they are too strong and thick, you drown.
 
+# you have failed your quest. (Loop back to entering the temple)
 
+# (User picks sundial)
+# Suddenly a secret door opens, you enter and are met with a straight path leading directly to another door, you feel suspicous of this, do you walk 
+# straight to the door, or investigate first?
+
+# 1. walk straight to the door
+# 2. investigate 
+
+# (User picks 1)
+# You decide that there would be no harm in walking straight to the door, you do so while thinking of what could be in that treasure chest, you are
+# intrupted mid-thought, when you trip and fall over something, its a trip-wire, the door on the other side of you closes, and lava starts pouring into the 
+# room, you die in the pool of lava.
+
+# You have failed your quest. (Loop back to entering the secret room)
+
+#(User picks 2)
+# You are very weary of the suspisously empty walkway, you thoroughly investigate the room and find a trip-wire, you carefully walk over it, and reach 
+# the other side, you wonder what would've happened if you hadn't investigated the room first, when you get to the other end, the door opens, you step
+# inside another room, this time, you see another stone with anceint writing, as you walk closer, it says, "if you can pass this test, you will be granted
+# what you most desire," below it reads, When the "door with no hinges" finally opens, what is the traveler said to discover?
+
+# 1. The shell
+# 2. The egg
+# 3. The yolk
+
+# (User picks 1) 
+# Suddenly treasure chests starts falling from above and crush you 
+
+# You have failed your quest. (Loop back to entering room)
+
+# (User picks 2)
+# Suddenly huge canons are lifted up, and treasure chests filled with treasure shoot out and hit you
+
+# You have failed your quest. (Loop back to entering the room)
+
+#(User picks 3)
+# Suddenly the wall behind the stone opens, revealing a treasure chest placed on a mount of gold and gems! Inside you see gold, gems a sword and the 
+# red gemstone!
+
+# Congratulations! You found all 4 gemstone and a full set of armor! You have completed you quest.
+
+# Now having found all 4 gemstones, you retire back to your homeland, on the way, you cant wait to tell everone about your adventure, but know your 
+# grandma will be angry that you touched her vase, still you are happy with the outcome you got from it. But there are still some questions thats 
+# weighing on your mind, what are these gemsstones? What do they do? Why are they directly North, West, South, and East of your home? (ETC)
+# You guess you'll find that out in your next adventure! 
+
+# (User picks compass)
+# without thinking, you hand the wondering arabic group your compass, they look at each other, then lauph, you ask them why, and they tell you to go into
+# the cave thats ontop of the "table mountain" beside you, you each exchange goodbyes, and then you head towards the mountaian, with great struggle, you
+# manage to climb ontop of it, you reach the cave the arab wonderers where talking about, and then go inside, once inside, a hungry mountain lion leaps
+# and attacks you, you couldn't react fast enough to reach and grab your weapons, you are eaten by the mountain lion.
+
+# you have failed your quest. (Loop back to talking to arab wonderers)
+
+# (User picks weapons)
+# You reach inside your backpack and hand them all the weapons you have, they nod in agreement and invite you to ride on one of their camels,
+# you hop on one, and they take you to an anceint temple, they tell you its filled with various traps and puzzles and no one has ever dared to step inside.
+# you thanked them and said goodbye and stepped inside the temple, the door closes from behind you, inside the temple is a stone with anceint arab
+# words written on it, it reads, "In the heat I stand both day and night, yet I never sweat, though the sun is bright, I point the way with shadows long-
+# guess my name and you prove strong," you take a minute to process the question, then come up with the anwser-
+
+# (If user picks camel)
+# Suddenly, a bunch of camels came out of a compartment and attacks you, you have no defence for yourself, you die from camels
+
+# You have failed your quest. (Loop back to talking with arab wonderers)
+
+# END OF SCENES!!! :D
 
 
 
