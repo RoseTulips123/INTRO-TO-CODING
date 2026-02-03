2847c885a0fc625dc966fad572f58a17bc05a3d5
JS <Jocelynn.Sergent@ela-academy.org>
2026-02-03 14:04:41 -0500

    Index fail

--- index.py
+++ index.py
@@ -16,35 +16,12 @@ print("You are a treasure hunter who is on a quest to find hidden treasures left
 # (Tutorial)
 print("\nType in the number to the choice you want to make.\n")
 
+chosen_direction = raw_input()
+
 # Choices:
-print("1. North (Treasure of Winter)" "\n"
-      "2. East  (Treasure of Autumn)" "\n"
-      "3. South (Treasure of Spring)" "\n"
-      "4. West  (Treasure of Summer)" "\n")
+def choose_direction():
 
 
-while True:
-    choice = input("What direction should go to?")
-
-    if choice == "1":
-        print("You set sail to the North!\n")
-        
-        
-    elif choice == "2":
-        print("You chose East (Treasure of Autumn)")
-        break
-    elif choice == "3":
-        print("You chose South (Treasure of Spring)")
-        break
-    elif choice == "4":
-        print("You chose West (Treasure of Summer)")
-        break
-    else:
-        print("Invalid choice. Please try again.")
-
-
-
-def Winter_island():
     print("You set sail to the North!\n")
     print("after days of sailing you reach an island directly North of your home, you dock your ship, " \
 "cold, arctic winds hit you face, good thing you prepared for this, you walk inland and see a huge " \
@@ -53,22 +30,27 @@ def Winter_island():
 "some snow from a fallen boulder next to it and see a blue gemstone engraved in the rock, you enter " \
 "the cave, its dark and cold, you walk deeper into the cave until you see a peice of meat laying " \
 "on the ground, you are very hungry from your voyage," "\n")
-    
+
 choice = input("Do you eat the meat?")   
 
 print("5. Eat the meat")
 print("6. Move on into the cave\n")
     
 
-def Meat_Scene():
-    print("You go to pick up the meat, as you do a huge polar bear jumps out from the shadows and attacks you, you try")
-    print("to fight it off but it overpowers you.\n")
-
-def Game_Over():
-    print("You have failed your quest.\n")
-
 if choice == "5":
-    return Meat_Scene()
+        print("You go to pick up the meat, as you do a huge polar bear jumps out from the shadows and attacks you, you try")
+        print("to fight it off but it overpowers you.\n")
+        print("You have failed your quest.")
+        
+
+if choice == "6":
+        print("You ignore the meat and walk deeper into the cave, after a few minutes you see a light in the distance, you")
+        print("walk towards it and find a large chamber with the floor frozen over, you see a large ice pedestal in the center")
+        print("of the chamber with a chest on top of it, should you cross the frozen floor to get to the chest? Or look around")
+        print("the chamber first?")
+
 else:
-    return Game_Over()
+    print("Invalid choice. Please try again.")
+
+
         
