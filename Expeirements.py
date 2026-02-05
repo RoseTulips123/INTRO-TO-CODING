903688f1ec70ce1df33b207973af6be6f0929037
JS <Jocelynn.Sergent@ela-academy.org>
2026-02-05 17:42:37 -0500

    Unfinished

--- Expeirements.py
+++ Expeirements.py
@@ -1,8 +1,19 @@
-print("after days of sailing you reach an island directly North of your home, you dock your ship, " \
-"cold, arctic winds hit you face, good thing you prepared for this, you walk inland and see a huge " \
-"mountain in the distance, you start to make your way to the mountain as big as mount everest, " \
-"after hours of hiking you reach the base of the mountain, you see a cave entrance, you brush off " \
-"some snow from a fallen boulder next to it and see a blue gemstone engraved in the rock, you enter " \
-"the cave, its dark and cold, you walk deeper into the cave until you see a peice of meat laying " \
-"on the ground, you are very hungry from your voyage, do you eat the meat?" "\n") 
+def introScene():
+    print("Example")
+    choice = input("Choose a, b, or c: ")
+    if choice == "a":
+        return scene1()
+    elif choice == "b":
+        return scene2()
+    elif choice == "c": 
+        return scene3()
+    
+    def scene1():
+       print("Scene 1")
+
+    def scene2():
+       print("Scene 2")
+
+    def scene3():
+       print("Scene 3")
 
--- index.py
+++ index.py
@@ -1,7 +1,7 @@
-# Treasure Quest Scenes
+
 print("⚔︎ Treasure Quest ⚔︎\n")
 
-# Intro description:
+
 print("You are a treasure hunter who is on a quest to find hidden treasures left by four ancient kings, its" \
 "been hidden for centeries and no one has been able to find it, but you wont give up! The only clue the ancient kings" \
 "left behind was a stone tablet with different symbols on it, you remember seeing these symbols on your grand mothers" \
@@ -12,35 +12,51 @@ print("You are a treasure hunter who is on a quest to find hidden treasures left
 "vase back then decide to set out on your quest to find the hidden treasures")
 
 
-# Game starts
-# (Tutorial)
+
 print("\nType in the number to the choice you want to make.\n")
 
-chosen_direction = raw_input()
+chosen_direction = input("What direction should you go first?\n"
+                         "1. North (Treasure of Winter)\n"
+                         "2. East  (Treasure of Autumn)\n"
+                         "3. South (Treasure of Spring)\n"
+                         "4. West  (Treasure of Summer)\n")
 
-# Choices:
+
+
 def choose_direction():
-
-
-    print("You set sail to the North!\n")
-    print("after days of sailing you reach an island directly North of your home, you dock your ship, " \
-"cold, arctic winds hit you face, good thing you prepared for this, you walk inland and see a huge " \
-"mountain in the distance, you start to make your way to the mountain as big as mount everest, " \
-"after hours of hiking you reach the base of the mountain, you see a cave entrance, you brush off " \
-"some snow from a fallen boulder next to it and see a blue gemstone engraved in the rock, you enter " \
-"the cave, its dark and cold, you walk deeper into the cave until you see a peice of meat laying " \
-"on the ground, you are very hungry from your voyage," "\n")
-
-choice = input("Do you eat the meat?")   
-
-print("5. Eat the meat")
-print("6. Move on into the cave\n")
+        if chosen_direction == "1":
+               print("You set sail to the North!\n")
+               return north_scene()
+        elif chosen_direction == "2":
+                print("You set sail to the East!\n")
+                return east_scene()
+        elif chosen_direction == "3":
+                print("You set sail to the South!\n")
+                return south_scene()
+        elif chosen_direction == "4":
+                print("You set sail to the West!\n")
+                return west_scene()
+        else:
+                print("Invalid choice. Please try again.")
+                choose_direction()
     
+def north_scene():    
+   print("after days of sailing you reach an island directly North of your home, you dock your ship,")
+print("cold, arctic winds hit you face, good thing you prepared for this, you walk inland and see a huge")
+print("mountain in the distance, you start to make your way to the mountain as big as mount everest,")
+print("after hours of hiking you reach the base of the mountain, you see a cave entrance, you brush off")
+print("some snow from a fallen boulder next to it and see a blue gemstone engraved in the rock, you enter")
+print("the cave, its dark and cold, you walk deeper into the cave until you see a peice of meat laying")
+print("on the ground, you are very hungry from your voyage," "\n")
+choice = input("Do you eat the meat?\n"
+               "5. Eat the meat\n"
+               "6. Move on into the cave\n")   
 
 if choice == "5":
         print("You go to pick up the meat, as you do a huge polar bear jumps out from the shadows and attacks you, you try")
         print("to fight it off but it overpowers you.\n")
         print("You have failed your quest.")
+        north_scene()
         
 
 if choice == "6":
@@ -50,7 +66,190 @@ if choice == "6":
         print("the chamber first?")
 
 else:
-    print("Invalid choice. Please try again.")
+        print("Invalid choice. Please try again.")
+        north_scene()
 
+choice = input("7. Cross the frozen floor to the chest\n"
+               "8. Look around the chamber first\n")
 
+if choice == "7":
+        print("You make your way across the frozen floor, as you near the pedestal you hear a cracking sound, before you can")
+        print("react the ice breaks beneath you, and you fall into freezing water below, the freezing temperature of the water")
+        print("gives you hypothermia and you drown.\n")
+        print("You have failed your quest.")
+        north_scene()
+
+if choice == "8":
+      print("You observe the ice closely and see that the floor is very thin in some spots, you carefully make your way across")
+      print("the chamber avoiding the thin ice, you reach the pedestal and open the chest, inside you find gold, diamonds,")
+      print("a sheild, and a Blue gemstone!\n")
+      print("Congratulations! You have found the Treasure of Winter!\n")
+
+choose_direction2 = input("What direction should you like to go to next?")
+print("9. East  (Treasure of Autumn)") 
+print("10. South (Treasure of Spring)") 
+print("11. West  (Treasure of Summer)\n") 
+
+def choose_direction2():
+        if choose_direction2 == "9":
+               print("You set sail to the East!\n")
+               return east_scene()
+        elif choose_direction2 == "10":
+                print("You set sail to the South!\n")
+                return south_scene()
+        elif choose_direction2 == "11":
+                print("You set sail to the West!\n")
+                return west_scene()
+        else:
+                print("Invalid choice. Please try again.")
+                choose_direction2()
+
+def east_scene():
+       print("Upon arriving at the island directly East of your home, you immediately see beautiful mountainous terrain") 
+       print("and colorful trees, you see two paths ahead of you, one leading into a beautiful forest, and one leading")
+       print("towards a treehouse village built into the mountainside, which path do you take?\n") 
+
+choice = input("Which path do you take?\n")
+print("12. Enter the forest")
+print("13. Head towards the treehouse village\n")
+
+def forest_path():  
+        print("You enter the forest, the trees are tall and the leaves are a beautiful array of reds, oranges, and yellows, as you")
+        print("walk deeper into the forest you see a clearing ahead, in the middle of the clearing you see an injured rare fox")
+        print("thats colored gold and red, do you help the fox, ignore it or take its fur for profit\n")
+
+def choice_path4():
+        choice = input("14. Help the fox" \
+                       "15. Ignore the fox" \
+                       "16. Take its fur for profit\n")
         
+        
+if choice == "14":
+        print("You carefully approach the injured fox, it looks at you with pleading eyes, you reach into you bag to find some")
+        print("bandages and medicine, luckily you packed extra supplies for your journey, you carefully bandage the fox's wounds")
+        print("and give it some water, after a few minutes the fox gets up and  hands you what it looks like a small map, the")
+        print("fox then runs off into the forest, you look at the map and see that it shows an X marking to a spot deeper in")
+        print("the forest, you decide to follow the map, after a few hours of searching you reach the spot marked on the map, you")
+        print("finnaly find the spot, you start digging and find a chest buried in the ground, you open the chest and find gold, jewels,")
+        print("a cape, and a yellow gemstone!\n")
+        print("Congratulations! You have found the Treasure of Autumn!\n")
+
+def choice_path3():
+  if choice == "12":
+       forest_path()       
+  elif choice == "13":
+       treehouse_path()       
+  else:
+      print("Invalid choice. Please try again.\n")
+      choice_path3()     
+
+
+if choice == "15":
+       print("You decide to ignore the fox and continue on your way, as you walk deeper into the forest, suddenly an tribe of")
+       print("hostile natives ambush you, they overpower you and take all your supplies, leaving you stranded in the forest with")
+       print("no food or water, after days of wandering you give up hope and succumb to starvation.\n")
+       print("You have failed your quest.\n")
+choice_path4()
+
+if choice == "16":
+        print("You see the foxes fur would be worth a ton of gold, you quickly grab your knife and skin the fox, you put the fur")
+        print("in your bag and turn back towards the path, as you walk back, a gigantic tree falls beside you, and before you")
+        print("could run, it crushes you.\n")
+        print("You have failed your quest.\n")
+        choice_path4()
+
+
+def treehouse_path():  
+       print("You decide that going to the treehouse village might have a better chance of finding the treasure, you make your")
+       print("way towards the village, as you get closer you see that the village is bustling with activity, people are going")
+       print("about their daily lives, you approach one of the villagers, they look at you like they've seen an alien, how ")
+       print("should you talk to them?\n")
+       choice = input("17. Try to communicate with gestures\n"  
+                      "18. Attempt to speak their language\n" 
+                      "19. Offer them a gift from your homeland\n")
+       
+       if choice ==   "17":
+              return communication_path()
+       elif choice == "18":
+              return language_path()
+       elif choice == "19":
+              return gift_path()
+       
+def communication_path():
+       print("You try to communicate with gestures, the villager looks confused but seems to understand you want to say, they"
+             "Motion for you to follow them, they lead you to the village elder, the elder listens to your story and decides to"
+             "help you, they give you a map with an X marking a spot deep in the nearby forest, you follow the map and after"
+             "a few hours of searching you reach the spot marked on the map, you start digging and find a chest buried in the"
+             "ground, inside you find gold, jewels, a cape, and a red gemstone!")
+       print("Congratulations! You have found the Treasure of Autumn!\n")
+       print("What direction should you go to next?")
+       direction_path3()
+
+def language_path():
+       print("You attempt to speak their language, but you only manage to butcher a few words, the villager looks at you angrily\n"
+            "and calls for others, soon you are surrounded by hostile villagers, they attack and kidnap you, you are held\n" 
+            "captive for weeks before you manage to escape, but you are left with nothing but the clothes on your back, you\n"
+            "eventually make your way back home dehydrated, starved, exhausted, and empty handed, you give up.\n")
+print("You have failed your quest.\n")
+choice_path3()
+
+def gift_path():
+       print("You offer the villager a gift from your homeland, they look at the gift curiously, after examining it they smile\n"
+             "and motion for you to follow them, they lead you to their hut, they give you food and water, you explain your\n"
+             "quest to them through gestures, they take you to the village elder, the villager explains your story to the\n"
+             "elder, the elder decides to help you, they give you a map with an \"x\" marking a spot deep in the nearby forest,\n"
+             "you follow the map and after a few hours of searching you reach the spot marked on the map, you start digging and\n"
+             "find a chest buried in the ground, inside you find gold, jewels, a cape, and a red gemstone!\n")
+       print("Congratulations! You have found the Treasure of Autumn!\n")
+       print("What direction should you go to next?")
+       direction_path3()
+
+def direction_path3():
+       direction = input("20. South (Treasure of Spring)\n"
+                         "21. West  (Treasure of Summer)\n")
+       
+       if direction == "20":
+              print("You set sail to the South!\n")
+              return south_scene()
+       elif direction == "21":          
+                return west_scene()
+       else:
+                print("Invalid choice. Please try again.")
+                direction_path3()
+
+
+def south_scene():
+       print("You decide that your next adventure will be the southern island, located directly south of your home, after a few")
+       print("days of sailing you reach the island, you dock you ship and step onto the beautiful, sandy beach, you see palm trees")
+       print("swaying in the warm breeze, beatiful lush greenery and exotic plants and animals you've never seen before, its like")
+       print("a paradise, you explore the beautiful lush island, and you manage to build an temporary shelter of palm leaves and")
+       print("wood. As night creeps over the island, you go to sleep, but you are then woken up by weird loud noises outside your")
+       print("shelter, do you investigate the noises, or stay in your shelter?\n")
+choice = input("22. Investigate the noises\n"
+               "23. Stay in your shelter\n")
+
+def choice_path5():
+       if choice == "22":
+        return investigate_path()
+if choice == "23":
+        return stay_path()
+else:
+       print("Invalid choice. Please try again.")
+       choice_path5()
+
+def investigate_path():
+       print("Curiosity gets the better of you and you decide to investigate the noises, you muster up the courage and step"
+      "outside your shelter, as you do, you are greeted by an unwelcomed guest of black tall creatures with glowing"
+      "purple eyes, you are stuck in a trance-like state, unable to move, you can only watch as the the figures purple"
+      "eyes get bigger and bigger, getting closer and closer, until you cant see no more.")
+       print("You have failed your quest.\n")
+       choice_path5()
+
+def stay_path():
+       
+
+
+
+
+
+
