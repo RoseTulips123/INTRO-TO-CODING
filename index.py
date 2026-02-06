6223fef273eab800e46074c046e7070348d5a1ca
JS <Jocelynn.Sergent@ela-academy.org>
2026-02-05 20:05:38 -0500

    Unfinished_2

--- index.py
+++ index.py
@@ -228,14 +228,7 @@ def south_scene():
 choice = input("22. Investigate the noises\n"
                "23. Stay in your shelter\n")
 
-def choice_path5():
-       if choice == "22":
-        return investigate_path()
-if choice == "23":
-        return stay_path()
-else:
-       print("Invalid choice. Please try again.")
-       choice_path5()
+
 
 def investigate_path():
        print("Curiosity gets the better of you and you decide to investigate the noises, you muster up the courage and step"
@@ -246,6 +239,178 @@ def investigate_path():
        choice_path5()
 
 def stay_path():
+       print("You decide to stay in your shelter, hoping the noises will go away, they eventually get farther away, until they are gone you cautiously"
+       "look outside your shelter, you see nothing but the night and stars, you go back to sleep, and awake to a beautiful sunny morning, you decide"
+       "to explore more of the island, as you walk deeper into the island, you name some of the exotic plants and animals, and jot them down in your"
+       "journal, after a few hours of exploring you reach a beautiful waterfall, and the stream flowing from it, you drink some out of the stream and"
+       "feel refreshed, you see a cave entrance behind the waterfall, do you enter the cave or rest by the waterfall.")
+
+def choice_path5():
+       if choice == "22":
+        return investigate_path()
+if choice == "23":
+       stay_path()
+        
+else:
+       print("Invalid choice. Please try again.")
+       choice_path5()
+
+choice = input("24. Enter the cave\n"
+               "25. Rest by the waterfall\n")
+
+def choice_path6():
+       if choice == "24":
+              return cave_path()
+       elif choice == "25":
+              return waterfall_path()
+       else:
+              print("Invalid choice. Please try again.")
+              choice_path6()
+
+def cave_path():
+       print("You are have heard stories of hidden treasures in caves behind waterfalls, so you decide to enter the cave, as you step inside bats"
+       "fly past you, the cave is dark and damp, you walk deeper into the cave, after a few minutes you reach a large chamber with walls"
+       "covered in colorful, glowing crystals, in the center you see a large glowing crystal with a treasure map inside it, you brought some tools"
+       "with you for your journey, witch tool should you use to break the crystal?")
+
+def choice_path7():
+       choice = input("26. Hammer\n"
+                      "27. Chisel\n"
+                      "28. Pickaxe\n")
+       return choice
+
+if choice == "26":
+       print("You use the hammer to try and break the crystal, you strike it with all your might, the crystal shatters into a million pieces, the" 
+       "shards fly everywhere, one sharp shard stabs you in the chest, you fall to the ground, helpless, as you die of loss of blood.")
+       print("You have failed your quest.\n")
+       choice_path6()
+
+if choice == "27":
+       print("You carefully use the chisel to break the crystal, after a few hours of careful work, you manage to break the crystal without damaging"
+         "the map inside, you unroll the map and see that it shows different landmarks on the island, with one landmark with an X marking, near"
+         "the center of the island, you follow the map, after an hour of walking, you reach the spot marked on the map, when you get to the place,"
+         "you see an entrance of an underground tunnel, you enter the tunnel, and find what it looks like a maze! You navigate through the maze using"
+         "the direction on the back of the map, you eventually reach a room at the end of the maze, with three paths leading towards the left, right,"
+         "and middle, which path do you take.")
+       
+choice = input("28. Left path\n"
+               "29. Middle path\n"
+               "30. Right path\n")
+
+def choice_path8():
+       if choice == "28":
+              return left_path()
+       elif choice == "29":
+              return middle_path()
+       elif choice == "30":
+              return right_path()
+       else:
+              print("Invalid choice. Please try again.")
+              choice_path8()
+
+def left_path():
+       print("You take the left path, after a few minutes of walking you reach a dead end, as you turn around to go back, you trigger a hidden trap,"
+         " the floor opens beneath you, and you fall into a pit of spikes.")
+print("You have failed your quest.\n")
+choice_path8()
+
+def middle_path():
+       print("You take the middle path, as it seems most convincing, you walk for a few minutes and at the end of the path you see a lever on the wall,"
+         " do you pull the lever or go back?")
+choice = input("31. Pull the lever\n"
+               "32. Go back\n")
+def choice_path9():
+       if choice == "31":
+              return lever_path()
+       elif choice == "32":
+              return middle_path()
+       else:
+              print("Invalid choice. Please try again.")
+              choice_path9()
+
+def lever_path():
+       print("You pull the lever, you hear a rumbling sound, the ceiling opens up then a large anvil falls from above, crushing you instantly.")
+       print("You have failed your quest.\n")
+choice_path9()
+
+def right_path():
+       print("You take the right path, the tunnel is long and narrow, after awhile, you hear echos of flowing water ahead, you walk faster and soon reach"
+              " a large underground waterfall, with a sparkling pool below, you see a chest at the base of the waterfall, you swim to the chest and open it,"
+              " inside you find gold, jewels, armor, and a pink gemstone!\n")
+       
+print("Congratulations! You have found the Treasure of Spring!\n")
+print("Would you like to go to the final direction?")
+
+def choice_path10():
+  choice = input("33. Yes\n"
+                 "34. No \n")
+choice_path10()
+
+def west_scene():
+       print("You decide that your final adventure will be the western island (Or whatever the user ends on), located directly west of your home," \
+       " after a few days of sailing you reach the island, you dock your ship and step onto the sandy desert, you see nothing but sand dunes and cacti" \
+       " as far as the eye can see, the sun is blazing hot, you feel thirsty by just looking at the dry desert, you scan the desert for any signs of life," \
+       " you see a rocky formation in the distance, you decide to head towards it, hoping to find some shade from the scorching sun, you make your way" \
+       " towards the rocky formation, When you get there, you see its a large mesa, with a few desert trees and plants, around it, you take shelter" \
+       " underneath one of the trees, you spot a group of arabs riding camels in the distance, you walk towards them, hoping they can help you, they" \
+       " see you and stop their camels, you luckily speak a good amount of their language from your previous travels, you explain your quest to them," \
+       " they say that they can help you but first you must give them something in return, what do you give them?")
+
+choice = input("35. Gold from previous treasures\n"
+               "36. Your compass\n"
+               "3. Your weapons\n")
+
+def choice_path11():
+       if choice == "35":
+              return gold_path()
+       elif choice == "36":
+              return compass_path()
+       elif choice == "37":
+              return weapons_path()
+       else:
+              print("Invalid choice. Please try again.")
+              choice_path11()
+
+def gold_path():
+       print("You reach inside your backpack and hand them the gold from previous adventures, they nod in agreement and invite you to ride on one of" \
+       " their camels, you hop on one, and they take you to an anceint temple, they tell you its filled with various traps and puzzles and no one has" \
+       " ever dared to step inside. you thanked them and said goodbye and stepped inside the temple, the door closes from behind you, inside the temple"
+       " is a stone with anceint arab words written on it, it reads, In the heat I stand both day and night, yet I never sweat, though the sun is bright," \
+       " I point the way with shadows long~ guess my name and you prove strong, you take a minute to process the question, then come up with the anwser~")
+
+
+
+
+
+if choice == "33":
+       west_scene()
+elif choice == "34":
+              print("Really? After all this work you dont want to finish your quest? Oh well, home you go.")
+              print("You have failed your quest.\n")
+              choice_path10()         
+else:
+       print("Invalid choice. Please try again.")
+       choice_path10()  
+
+       
+if choice == "28":
+       print("You use the pickaxe to try and break the crystal, you strike it a little too hard, the crystal breaks in half, and so does the map"
+         "inside, you are left with nothing but shattered glass and torn paper.")
+print("You have failed your quest.\n")
+choice_path7()
+
+
+
+
+
+
+
+
+
+
+
+
+       
        
 
 
