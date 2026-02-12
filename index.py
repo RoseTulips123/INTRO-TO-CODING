10467b20c49fc693a0d9ad32d03df34281b9d1d5
JS <Jocelynn.Sergent@ela-academy.org>
2026-02-12 16:33:01 -0500

    ignore

--- index.py
+++ index.py
@@ -498,11 +498,12 @@ def yolk_path():
       inventory.append("Sword", "Red gemstone")
       print("Congratulations! You found all 4 gemstone and a full set of armor! You have completed you quest.\n \n")
 
-
-print("\"Now having found all 4 gemstones, you retire back to your homeland, on the way, you cant wait to tell everone about your adventure, but"
+def outro_scene():
+       print("\"Now having found all 4 gemstones, you retire back to your homeland, on the way, you cant wait to tell everone about your adventure, but"
 " know your grandma will be angry that you touched her vase, still you are happy with the outcome you got from it. But there are still some questions"
 " thats weighing on your mind, what are these gemsstones? What do they do? Why are they directly North, West, South, and East of your home? (ETC)"
 " You guess you'll find that out in your next adventure!")
+
 
 # last paths/scenes from 11th choice path:
 
